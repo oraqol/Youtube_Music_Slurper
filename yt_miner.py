45e2e8b8e22349f82511b08588616cc22f452d8f
oraqol <Johan Giraldo>
2022-02-03 19:51:55 -0500

    stuff

--- yt_miner.py
+++ yt_miner.py
@@ -2,33 +2,46 @@
 
 from ytmusicapi import YTMusic
 from random import randint
-import argparse
-import json
 import os
 import subprocess
 import time
-import pandas as pd
+import sys
+import touch
 
 def downloader(ex_artist, ex_album, ex_url):
     print(f'/homepool/music/discographies/{ex_artist}/{ex_album}')
     if not os.path.exists(f'/homepool/music/discographies/{ex_artist}/{ex_album}'):
         os.makedirs(f'/homepool/music/discographies/{ex_artist}/{ex_album}')
     subprocess.call(f"/usr/local/bin/yt-dlp --default-search 'ytsearch' -i --yes-playlist --restrict-filenames -R 3 --add-metadata --extract-audio --audio-quality 0 --audio-format mp3 --prefer-ffmpeg --embed-thumbnail -f 'bestaudio' --download-archive /homepool/music/discographies/youtube_dl.archive -o '/homepool/music/discographies/{ex_artist}/{ex_album}/%(title)s - %(artist)s - %(album)s.%(ext)s' {ex_url}", shell=True)
-time.sleep(randint(1,120))
+#    time.sleep(randint(1,120))
 
 def append_spent_artists(name_of_artist, list_of_spent_artists):
         list_of_spent_artists = open("/etc/yt_music/yt_sync.artist_log", "a")
         list_of_spent_artists.write(f"{name_of_artist}\n")
         list_of_spent_artists.close()
 
+flag = sys.argv[1]
 yt = YTMusic('/etc/yt_music/headers_auth.json', '104862870707763213449')
 liked_songs = yt.get_liked_songs(10000000)
 playlists = yt.get_library_playlists(100000)
 artist_array = []
 liked_song_array = []
+spent_artists = []
 
 with open('/etc/yt_music/yt_sync.config') as f:
         playlist_name = f.readlines()[0].strip()
+
+if flag == '--full-scan':
+    if os.path.exists('/etc/yt_music/yt_sync.artist_log'):
+        os.remove('/etc/yt_music/yt_sync.artist_log')
+elif flag == '--light-scan':
+    with open('/etc/yt_music/yt_sync.artist_log') as file:
+        spent_artists = [line.rstrip() for line in file]
+else:
+    print('Flag missing: --full-scan or --light-scan')
+    quit()
+
+touch.touch('/etc/yt_music/yt_sync.artist_log')
 
 playlist_id = None
 
@@ -38,10 +51,6 @@ for playlist in playlists:
 
 if playlist_id == None:
     print(f'{playlist_name} not found')
-
-spent_artists = []
-with open('/etc/yt_music/yt_sync.artist_log') as file:
-    spent_artists = [line.rstrip() for line in file]
 
 public_song_array = []
 public_songs = yt.get_playlist(playlist_id, 100000000)
@@ -63,16 +72,19 @@ for artist in artist_array:
     artist_name = artist['name'].strip()
 #    if not artist_name == 'Blue Öyster Cult':
 #        continue
-    if artist_name in spent_artists:
+    if flag == '--light-scan':
+        if artist_name in spent_artists:
 #        print(f'{artist_name} has already been processed. Skipping...')
-        continue
+            continue
 
     exists = used_ids.count(artist_id)
+
     if exists > 0:
         print(f'{artist_name} has already been processed. Skipping...')
         dupe_ids.append([artist_name, artist_id])
         append_spent_artists(artist_name, spent_artists)
         continue
+
     used_ids.append(artist_id)
 
     try:
@@ -101,7 +113,6 @@ for artist in artist_array:
     try:
         print('\n\nLooping through singles...')
         singles = yt.get_artist(channel_id)['singles']['results']
-        print(singles)
         for single in singles:
             single_title = single['title']
             single_browse_id = single['browseId']
